8f1029143494db5263aaa383df650cf1af6e4199
VALDEMIR GOMES <valdemirg1000@gmail.com>
2024-08-18 18:36:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.getElementById('encrypt-btn').addEventListener('click', function() {
     let inputText = document.getElementById('input-text').value;
     let encryptedText = inputText
-        .replace(/a/g, 'ai')
         .replace(/e/g, 'enter')
         .replace(/i/g, 'imes')
+        .replace(/a/g, 'ai')
         .replace(/o/g, 'ober')
         .replace(/u/g, 'ufat');
     document.getElementById('output-text').value = encryptedText;
@@ -12,9 +12,9 @@ document.getElementById('encrypt-btn').addEventListener('click', function() {
 document.getElementById('decrypt-btn').addEventListener('click', function() {
     let inputText = document.getElementById('input-text').value;
     let decryptedText = inputText
-        .replace(/ai/g, 'a')
         .replace(/enter/g, 'e')
         .replace(/imes/g, 'i')
+        .replace(/ai/g, 'a')
         .replace(/ober/g, 'o')
         .replace(/ufat/g, 'u');
     document.getElementById('output-text').value = decryptedText;
